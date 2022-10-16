24317dee797598db468e0d1ed7bce0361574a391
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-10-17 00:24:44 +0800

    Admin Bite list

--- src/pages/admin/editBite/AdminBiteList.tsx
+++ src/pages/admin/editBite/AdminBiteList.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { biteAction } from "../../../redux/actions/biteActions"
+import "../../../assets/styles/admin/editBite/AdminBiteListStyle.scss"
 
 const AdminBiteList = () => {
     const navigate = useNavigate()
@@ -12,8 +13,29 @@ const AdminBiteList = () => {
     useEffect(() => { dispatch(biteAction.getAllBites()) }, [])
 
     return (
-        <div>
-
+        <div className="admin-bite-list-wrapper">
+            <div className="bite-list">
+                <table className="data-table">
+                    <thead>
+                        <tr>
+                            <th>Post Date</th>
+                            <th>Post Time</th>
+                            <th>Bite title</th>
+                            <th>author</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {bites.map((bite: any, index: any) => (
+                            <tr key={index}>
+                                <td>{new Date(bite.date).toUTCString().slice(5, 16)}</td>
+                                <td>{new Date(bite.date).toUTCString().slice(17, 25)}</td>
+                                <td>{bite.title}</td>
+                                <td>{bite.owner.name}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
