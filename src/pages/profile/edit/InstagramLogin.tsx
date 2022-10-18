2eea37feb7f19dc23e0a6427be855b6fed26219e
Monirul Islam <Monirul Islam>
2022-10-18 14:59:35 +0600

    instagram login design fixed

--- src/pages/profile/edit/InstagramLogin.tsx
+++ src/pages/profile/edit/InstagramLogin.tsx
@@ -1,103 +1,103 @@
-import { useCallback, useEffect, useMemo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
-import { accountAction } from '../../../redux/actions/socialAccountActions'
+import { useCallback, useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
+import { accountAction } from '../../../redux/actions/socialAccountActions';
 
 export const InstagramLogin = () => {
-    const [searchParams] = useSearchParams()
-    const { pathname } = useLocation()
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const userState = useSelector((state: any) => state.auth)
-    const accountState = useSelector((state: any) => state.accounts)
-    const { accounts } = accountState
-    const { user } = userState
+  const [searchParams] = useSearchParams();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userState = useSelector((state: any) => state.auth);
+  const accountState = useSelector((state: any) => state.accounts);
+  const { accounts } = accountState;
+  const { user } = userState;
 
-    const clickHandler = () => {
-        const clientId = process.env.REACT_APP_INSTAGRAM_APP_ID
-        const scope = process.env.REACT_APP_INSTAGRAM_SCOPE
-        const responseType = 'code'
-        const redirectUri = window.location.origin + window.location.pathname
-        window.location.href = `https://api.instagram.com/oauth/authorize/?app_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`
+  const clickHandler = () => {
+    const clientId = process.env.REACT_APP_INSTAGRAM_APP_ID;
+    const scope = process.env.REACT_APP_INSTAGRAM_SCOPE;
+    const responseType = 'code';
+    const redirectUri = `https://${window.location.host}${window.location.pathname}`;
+    window.location.href = `https://api.instagram.com/oauth/authorize/?app_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
+  };
+
+  const removeHandler = () => {
+    const { _id } = accounts.find((acc: any) => acc.name === 'instagram');
+    dispatch(accountAction.removeAccount(_id));
+  };
+
+  const hasInstagram: any = useMemo(() => {
+    const d = accounts.find((acc: any) => acc.name === 'instagram');
+    return d && Object.keys(d).length > 0;
+  }, [accounts]);
+
+  const onSuccess = useCallback(
+    async (code) => {
+      const redirect_uri = `https://${window.location.host}${window.location.pathname}`;
+      const meta = {
+        code,
+        redirect_uri,
+        grant_type: 'authorization_code',
+      };
+      console.log(meta.redirect_uri);
+      const data = {
+        id: code,
+        name: 'instagram',
+        metadata: JSON.stringify(meta),
+      };
+      dispatch(
+        accountAction.addAccount(data, (result: Boolean) => {
+          if (result) {
+            // dispatch(accountAction.getAccounts(user.id));
+            setTimeout(() => {
+              navigate(pathname);
+            }, 2000);
+          }
+        })
+      );
+    },
+    [dispatch, pathname, navigate]
+  );
+
+  const openInstagramProfile = () => {
+    const data = accounts.find((acc: any) => acc.name === 'instagram');
+    const { username } = JSON.parse(data.metadata);
+    window.open(`https://www.instagram.com/${username}`);
+  };
+
+  const onFailure = useCallback(() => {
+    if (searchParams.get('error')) {
+      const data = {
+        error: searchParams.get('error'),
+        error_reason: searchParams.get('error_reason'),
+        error_description: searchParams.get('error_description'),
+      };
+      console.log(data);
     }
+  }, [searchParams]);
 
-    const removeHandler = () => {
-        const { _id } = accounts.find((acc: any) => acc.name === 'instagram')
-        dispatch(accountAction.removeAccount(_id))
+  useEffect(() => {
+    const code = searchParams.get('code');
+    if (code) onSuccess(code);
+    else onFailure();
+  }, [onSuccess, onFailure, searchParams, dispatch, user]);
+  useEffect(() => {
+    if (user && 'id' in user) {
+      dispatch(accountAction.getAccounts(user?.id));
     }
+  }, [dispatch, user]);
 
-    const hasInstagram: any = useMemo(() => {
-        const d = accounts.find((acc: any) => acc.name === 'instagram')
-        return d && Object.keys(d).length > 0
-    }, [accounts])
+  return (
+    <>
+      {hasInstagram && <div onClick={openInstagramProfile}>View</div>}
+      <div
+        className={hasInstagram ? 'remove-btn' : 'connect-btn'}
+        onClick={hasInstagram ? removeHandler : clickHandler}
+      >
+        <span>{hasInstagram ? 'Remove' : 'Connect'}</span>
+      </div>
+    </>
+  );
+};
 
-    const onSuccess = useCallback(
-        async (code) => {
-            const isLocal = true
-            const redirect_uri = () => {
-                const { protocol, host, pathname, origin } = window.location
-                if (!isLocal) return origin + pathname
-                const newProtocol = protocol === 'http' ? 'http' : 'https'
-                return `${newProtocol}://${host}${pathname}`
-            };
-            const meta = {
-                code,
-                redirect_uri: redirect_uri(),
-                grant_type: 'authorization_code',
-            };
-            console.log(meta.redirect_uri)
-            const data = {
-                id: code,
-                name: 'instagram',
-                metadata: JSON.stringify(meta),
-            };
-            dispatch(
-                accountAction.addAccount(data, (result: Boolean) => {
-                    if (result) {
-                        dispatch(accountAction.getAccounts(user.id))
-                        setTimeout(() => {
-                            navigate(pathname)
-                        }, 2000)
-                    }
-                })
-            );
-        },
-        [dispatch, pathname, navigate]
-    );
-
-    const openInstagramProfile = () => {
-        const data = accounts.find((acc: any) => acc.name === 'instagram')
-        const { username } = JSON.parse(data.metadata)
-        window.open(`https://www.instagram.com/${username}`)
-    };
-
-    const onFailure = useCallback(() => {
-        if (searchParams.get('error')) {
-            const data = {
-                error: searchParams.get('error'),
-                error_reason: searchParams.get('error_reason'),
-                error_description: searchParams.get('error_description'),
-            };
-            console.log(data)
-        }
-    }, [searchParams])
-
-    useEffect(() => {
-        const code = searchParams.get('code');
-        if (code) onSuccess(code)
-        else onFailure()
-    }, [onSuccess, onFailure, searchParams])
-
-    return (
-        <>
-            {hasInstagram && <div onClick={openInstagramProfile}>View</div>}
-            <div className={hasInstagram ? 'remove-btn' : 'connect-btn'}
-                onClick={hasInstagram ? removeHandler : clickHandler}
-            >
-                <span>{hasInstagram ? 'Remove' : 'Connect'}</span>
-            </div>
-        </>
-    )
-}
-
-export default InstagramLogin
+export default InstagramLogin;
--- src/pages/profile/edit/socialAccount.tsx
+++ src/pages/profile/edit/socialAccount.tsx
@@ -1,107 +1,116 @@
-import { useEffect, useMemo } from "react"
-import { useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
-import { GoogleLogin } from "react-google-login"
-import axios from "axios"
-import { InstagramIcon, YoutubeIcon } from "../../../assets/svg"
-import InstagramLogin from "./InstagramLogin"
-import { BackIcon } from "../../../assets/svg"
-import { accountAction } from "../../../redux/actions/socialAccountActions"
-import "../../../assets/styles/profile/socialAccountStyle.scss"
+import axios from 'axios';
+import { useEffect, useMemo } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import '../../../assets/styles/profile/socialAccountStyle.scss';
+import { BackIcon, InstagramIcon, YoutubeIcon } from '../../../assets/svg';
+import { accountAction } from '../../../redux/actions/socialAccountActions';
+import InstagramLogin from './InstagramLogin';
 
 const Socialaccount = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const userState = useSelector((state: any) => state.auth)
-  const accountState = useSelector((state: any) => state.accounts)
-  const { accounts } = accountState
-  const { user } = userState
-
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const userState = useSelector((state: any) => state.auth);
+  const accountState = useSelector((state: any) => state.accounts);
+  const { accounts } = accountState;
+  const { user } = userState;
 
   const openConnectedYoutubeChannel = () => {
     const { id } = accounts.find((acc: any) => acc.name === 'youtube');
     window.open(`https://www.youtube.com/channel/${id}`);
-  }
+  };
 
   const hasYoutubeData: any = useMemo(() => {
-    if (accounts.length <= 0) return false
-    const d = accounts.find((acc: any) => acc.name === 'youtube')
-    return d && Object.keys(d).length > 0
-  }, [accounts])
+    if (accounts.length <= 0) return false;
+    const d = accounts.find((acc: any) => acc.name === 'youtube');
+    return d && Object.keys(d).length > 0;
+  }, [accounts]);
 
   const removeYoutube = () => {
-    const youtubeData = accounts.find((acc: any) => acc.name === 'youtube')
+    const youtubeData = accounts.find((acc: any) => acc.name === 'youtube');
     if (youtubeData && Object.keys(youtubeData).length > 0) {
-      dispatch(accountAction.removeAccount(youtubeData._id))
+      dispatch(accountAction.removeAccount(youtubeData._id));
     }
-  }
+  };
 
-  useEffect(() => { if (user) dispatch(accountAction.getAccounts(user.id)) }, [dispatch, user])
+  useEffect(() => {
+    if (user) dispatch(accountAction.getAccounts(user.id));
+  }, [dispatch, user]);
 
   const responseGoogleSuccess = async (response: any) => {
     try {
-      const access_token = response.accessToken
-      const youtubeApiUrl = `https://youtube.googleapis.com/youtube/v3/channels?part=id&mine=true&access_token=${access_token}&key=${process.env.REACT_APP_GOOGLE_CLIENT_ID}`
-      const response1 = await axios.get(youtubeApiUrl)
+      const access_token = response.accessToken;
+      const youtubeApiUrl = `https://youtube.googleapis.com/youtube/v3/channels?part=id&mine=true&access_token=${access_token}&key=${process.env.REACT_APP_GOOGLE_CLIENT_ID}`;
+      const response1 = await axios.get(youtubeApiUrl);
       const data = {
         id: response1.data.items[0].id,
         name: 'youtube',
         metadata: JSON.stringify(response1.data.items),
       };
-      dispatch(accountAction.addAccount(data))
+      dispatch(accountAction.addAccount(data));
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   return (
-    <div className="social-accounts-wrapper">
-      <div className="page-header">
-        <div onClick={() => navigate('/myaccount/edit')}><BackIcon color="black" /></div>
-        <div className="page-title"><span>Social Accounts</span></div>
+    <div className='social-accounts-wrapper'>
+      <div className='page-header'>
+        <div onClick={() => navigate('/myaccount/edit')}>
+          <BackIcon color='black' />
+        </div>
+        <div className='page-title'>
+          <span>Social Accounts</span>
+        </div>
         <div style={{ width: '24px' }}></div>
       </div>
-      <div className="social-accounts">
-        <div className="socialaccounts">
-          <div className="content">
-            <div className="icon-title">
-              <div className="icon">
-                <InstagramIcon color="#EFA058" />
+      <div className='social-accounts'>
+        <div className='socialaccounts'>
+          <div className='content'>
+            <div className='icon-title'>
+              <div className='icon'>
+                <InstagramIcon color='#EFA058' />
               </div>
-              <div className="title">Instagram</div>
+              <div className='title'>Instagram</div>
             </div>
-            <div className="btn">
-              <InstagramLogin />
-            </div>
+
+            <InstagramLogin />
           </div>
-          <div className="content">
-            <div className="icon-title">
-              <div className="icon">
-                <YoutubeIcon color="#EFA058" />
+          <div className='content'>
+            <div className='icon-title'>
+              <div className='icon'>
+                <YoutubeIcon color='#EFA058' />
               </div>
-              <div className="title">Youtube</div>
+              <div className='title'>Youtube</div>
             </div>
-            {hasYoutubeData && <div onClick={openConnectedYoutubeChannel}>View</div>}
-            <div className="btn">
-              {hasYoutubeData ?
-                <div className="remove-btn" onClick={removeYoutube}><span>Remove</span></div>
-                :
+            {hasYoutubeData && (
+              <div onClick={openConnectedYoutubeChannel}>View</div>
+            )}
+            <div className='btn'>
+              {hasYoutubeData ? (
+                <div className='remove-btn' onClick={removeYoutube}>
+                  <span>Remove</span>
+                </div>
+              ) : (
                 <GoogleLogin
                   clientId={`${process.env.REACT_APP_GOOGLE_CLIENT_ID}`}
                   render={(renderProps) => (
-                    <div className="connect-btn" onClick={renderProps.onClick}><span>Connect</span></div>
+                    <div className='connect-btn' onClick={renderProps.onClick}>
+                      <span>Connect</span>
+                    </div>
                   )}
                   onSuccess={responseGoogleSuccess}
-                  cookiePolicy={"single_host_origin"}
+                  cookiePolicy={'single_host_origin'}
                   scope='https://www.googleapis.com/auth/youtube.readonly'
                 />
-              }
+              )}
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Socialaccount
+export default Socialaccount;
