da47a16657638afb096ad9d22b62f42b9870f5a5
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-10-23 13:47:18 +0800

    Bites

--- src/pages/profile/Bites.tsx
+++ src/pages/profile/Bites.tsx
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { useLocation, useNavigate } from "react-router-dom"
 import ProfileMenu from "../../components/profileMenu"
 import BiteCardProfile from "../../components/bite/BiteCardProfile"
-import PurchaseModal from "../../components/modals/PurchaseModal"
-import UnLockFreeModal from "../../components/modals/UnLockFreeModal"
 import { biteAction } from "../../redux/actions/biteActions"
 import { BackIcon } from "../../assets/svg"
-import { SET_DIALOG_STATE } from "../../redux/types"
 import '../../assets/styles/profile/creatorListStyle.scss'
 
 const Bites = () => {
@@ -15,28 +12,9 @@ const Bites = () => {
     const dispatch = useDispatch()
     const location = useLocation()
     const biteState = useSelector((state: any) => state.bite)
-    const loadState = useSelector((state: any) => state.load)
-    const userState = useSelector((state: any) => state.auth)
-    const [bite, setBite] = useState<any>(null)
     const { bites } = biteState
-    const { dlgState } = loadState
-    const { user } = userState
-
-    const [openFreeUnlock, setOpenFreeUnLock] = useState(false)
-    const [openPurchaseModal, setOpenPurchaseModal] = useState(false)
-    const [openPaymentForm, setOpenPaymentForm] = useState(false)
 
     useEffect(() => { dispatch(biteAction.getBitesList()) }, [location, dispatch])
-    useEffect(() => {
-        if (dlgState === 'unlock_free') setOpenFreeUnLock(true)
-    }, [dlgState])
-
-    useEffect(() => {
-        if (bite) {
-            if (bite.currency) setOpenPurchaseModal(true)
-            else dispatch(biteAction.unLockBite(bite._id, bite.currency, bite.price, null))
-        }
-    }, [bite])
 
     return (
         <div className="creator-wrapper">
@@ -45,32 +23,6 @@ const Bites = () => {
                 <div className="page-title"><span>List of Bites</span></div>
                 <div style={{ width: '24px' }}></div>
             </div>
-            <PurchaseModal
-                show={openPurchaseModal}
-                onClose={() => {
-                    setOpenPurchaseModal(false)
-                    setBite(null)
-                }}
-                bite={bite}
-                handleSubmit={() => {
-                    if (user) {
-                        setOpenPurchaseModal(false)
-                        setOpenPaymentForm(true)
-                    }
-                }}
-            />
-            <UnLockFreeModal
-                show={openFreeUnlock}
-                onClose={() => {
-                    setOpenFreeUnLock(false)
-                    dispatch({ type: SET_DIALOG_STATE, payload: "" })
-                }}
-                bite={bite}
-                handleSubmit={() => {
-                    dispatch({ type: SET_DIALOG_STATE, payload: "" })
-                    navigate(`/${user.personalisedUrl}`)
-                }}
-            />
             <div className="list-menu">
                 <ProfileMenu
                     selectedText="Bites"
@@ -83,7 +35,7 @@ const Bites = () => {
                     <div className="bite" key={index}>
                         <BiteCardProfile
                             bite={bite}
-                            setBite={setBite} same={false}
+                            same={false}
                         />
                     </div>
                 ))}
