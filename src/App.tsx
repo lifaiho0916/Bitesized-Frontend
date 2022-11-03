e2c6083c9f3dba85d12b5045d13cb7abcc7df9c1
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-11-03 13:25:10 +0800

    quick Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter as Router} from "react-router-dom";
-import AppRoutes from "./routes";
-import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom"
+import AppRoutes from "./routes"
+import "./App.css"
 
 const App = () => {
-  
+
   return (
     <Router>
-        <AppRoutes />
+      <AppRoutes />
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
