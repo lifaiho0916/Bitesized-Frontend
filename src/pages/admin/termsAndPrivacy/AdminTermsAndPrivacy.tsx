cbcdc2c39ef1a69e5e0a2d526a0fd46d10f67570
PhoenixDev1223 <106264352+YacobGadAI@users.noreply.github.com>
2022-11-14 19:25:55 -0500

    TandCadmin Complete

--- src/pages/admin/termsAndPrivacy/AdminTermsAndPrivacy.tsx
+++ src/pages/admin/termsAndPrivacy/AdminTermsAndPrivacy.tsx
@@ -2,65 +2,47 @@ import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Editor } from "react-draft-wysiwyg";
-// import { DraftBlockType, DraftBlockRenderConfig, Editor, EditorState } from "draft-js";
-import draftToHtml from 'draftjs-to-html';
-import Immutable from "immutable"
+import draftToHtml from "draftjs-to-html";
 import Button from "../../../components/general/button";
 import { settingAction } from "../../../redux/actions/settingActions";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "../../../assets/styles/admin/termsAndPrivacy/AdminTermsAndPrivacyStyle.scss";
 
-const myBlockStyleFn = (contentBlock: any) => {
-  const type = contentBlock.getType()
-  console.log(type)
-  if (type === 'header-one') {
-    console.log("adsfsda")
-    return 'superFancyBlockquote';
-  }
-}
-
 const AdminTermsAndPrivacy = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const dispatch = useDispatch()
-  const settingState = useSelector((state: any) => state.setting)
-  const { termsAndPrivacy } = settingState
+  const dispatch = useDispatch();
+  const settingState = useSelector((state: any) => state.setting);
+  const { termsAndPrivacy } = settingState;
   const [searchParams] = useSearchParams();
   const code = searchParams.get("type");
-  const [initialState, setInitialState] = useState<any>(null)
-  const [changeState, setChangeState] = useState<any>(null)
-  // const [editorState, setEditorState] = useState<any>(EditorState.createEmpty())
-
-  const onContentStateChange = (state: any) => {
-    setChangeState(state)
-  }
-
-  // const onEditorStateChange = (state: any) => { setEditorState(state) }
+  const [initialState, setInitialState] = useState<any>(null);
+  const [changeState, setChangeState] = useState<any>(null);
 
   const save = () => {
     const payload = {
       terms: code === "terms" ? changeState : termsAndPrivacy.terms,
-      privacy: code === "privacy" ? changeState : termsAndPrivacy.privacy
-    }
-    dispatch(settingAction.saveTermAndPrivacy(payload))
-  }
+      privacy: code === "privacy" ? changeState : termsAndPrivacy.privacy,
+    };
+    dispatch(settingAction.saveTermAndPrivacy(payload));
+  };
 
   useEffect(() => {
-    if (code === null) navigate(`${location.pathname}?type=terms`)
+    if (code === null) navigate(`${location.pathname}?type=terms`);
     else {
       if (code === "terms") {
-        setInitialState(termsAndPrivacy ? termsAndPrivacy?.terms : null)
-        setChangeState(termsAndPrivacy ? termsAndPrivacy?.terms : null)
+        setInitialState(termsAndPrivacy ? termsAndPrivacy?.terms : null);
+        setChangeState(termsAndPrivacy ? termsAndPrivacy?.terms : null);
       } else {
-        setInitialState(termsAndPrivacy ? termsAndPrivacy?.privacy : null)
-        setChangeState(termsAndPrivacy ? termsAndPrivacy?.privacy : null)
+        setInitialState(termsAndPrivacy ? termsAndPrivacy?.privacy : null);
+        setChangeState(termsAndPrivacy ? termsAndPrivacy?.privacy : null);
       }
     }
-  }, [code, termsAndPrivacy])
+  }, [code, termsAndPrivacy]);
 
   useEffect(() => {
-    dispatch(settingAction.getTermsAndPrivacy())
-  }, [dispatch])
+    dispatch(settingAction.getTermsAndPrivacy());
+  }, [dispatch]);
 
   return (
     <div className="admin-terms-and-privacy-wrapper">
@@ -87,42 +69,75 @@ const AdminTermsAndPrivacy = () => {
             />
           </div>
         </div>
-        {/* <Editor
-          editorState={editorState}
-          onChange={onEditorStateChange}
-        /> */}
         <Editor
           contentState={initialState}
-          onContentStateChange={onContentStateChange}
+          onContentStateChange={setChangeState}
           toolbarClassName="toolbarClassName"
           wrapperClassName="wrapperClassName"
           editorClassName="editor-style"
-          
-          customBlockRenderFunc={myBlockStyleFn}
           toolbar={{
-            options: ['blockType', 'inline', 'list', 'textAlign', 'link'],
+            options: [
+              "blockType",
+              "inline",
+              "fontSize",
+              "fontFamily",
+              "list",
+              "textAlign",
+              "colorPicker",
+              "link",
+            ],
+            blockType: {
+              inDropdown: true,
+              options: ["Normal", "H1", "H2", "H3", "H4", "H5", "H6"],
+              className: undefined,
+              component: undefined,
+              dropdownClassName: undefined,
+            },
             inline: {
               inDropdown: false,
-              options: ['bold', 'italic', 'underline', 'strikethrough'],
+              options: ["bold", "italic", "underline"],
+            },
+            fontSize: {
+              options: [
+                8, 9, 10, 11, 12, 14, 16, 18, 20, 24, 30, 36, 48, 60, 72, 96,
+              ],
+              className: undefined,
+              component: undefined,
+              dropdownClassName: undefined,
+            },
+            fontFamily: {
+              options: ["Lato"],
+              className: undefined,
+              component: undefined,
+              dropdownClassName: undefined,
             },
             list: {
               inDropdown: false,
-              options: ['unordered', 'ordered'],
+              options: ["unordered", "ordered"],
             },
             textAlign: {
               inDropdown: false,
-              options: ['left', 'center', 'right', 'justify'],
+              options: ["left", "center", "right", "justify"],
+            },
+            colorPicker: {
+              // icon: color,
+              className: undefined,
+              component: undefined,
+              popupClassName: undefined,
+              colors: ["#EFA058", "#000000"],
             },
             link: {
               inDropdown: false,
               showOpenOptionOnHover: true,
-              defaultTargetOption: '_self',
-              options: ['link', 'unlink'],
-            }
+              defaultTargetOption: "_self",
+              options: ["link", "unlink"],
+            },
           }}
         />
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
         <Button
           text="Save"
           fillStyle="fill"
