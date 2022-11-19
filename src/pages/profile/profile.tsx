529dcc31f7afe1fe382e75b67c90ca107d6fe88a
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-11-19 12:21:31 +0800

    Profile Page Update

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -61,105 +61,105 @@ const Profile = () => {
             /> */}
           </div>
         }
-            {code === "subscription" ? 
-              <div className="subscription">
-                {(user && user.subscribe.available === true && user.subscribe.switch === true) &&
-                <>
-                  <div className="subscription-title">
-                    <CreatoCoinIcon color="#EA8426" /><span>Manage my subscription</span>
-                  </div>
-                  <div className="subscription-body">
-                    <div className="subscription-body-title">
-                      <span>Subscription</span>
-                    </div>
-                    <div className="subscription-body-main">
-                      <div className="image">
-                      <img src={subscriptionImg} alt="subscription" />
-                      </div>
-                      <div className="description">
-                        <span>You can provide subscription-only benefits for subscribe members, such as additional creative content, subscription to member-only communities, etc.</span>
-                        <div className="subscription-btn">
-                          <span>Set subscription</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </>
-                }
+          {code === "subscription" ? 
+            <div className="subscription">
+              {(user && user.subscribe.available === true && user.subscribe.switch === true) &&
+              <>
                 <div className="subscription-title">
-                  <NotificationOutlineIcon color="#EA8426" width={24} /><span>I have subscribed</span>
+                  <CreatoCoinIcon color="#EA8426" /><span>Manage my subscription</span>
                 </div>
-              </div>
-              :
-            <div className="creators-bite">
-            {(!isSame && authuser) &&
-              <div className="title">
-                <Bite1Icon color="#EFA058" width={30} height={30} />
-                <span>{authuser.name}’s Bite-sized knowledge</span>
-              </div>
-            }
-              {bites.length > 0 && authuser ? (
-                <div className="bite-card">
-                  {bites.sort((first: any, second: any) => {
-                      if (user) {
-                        let firstDate: any = "2322";
-                        let secondDate: any = "2322";
-                        let firstIndex = first.purchasedUsers.findIndex((purchaseInfo: any) => String(purchaseInfo.purchasedBy) === String(user.id));
-                        let secondIndex = second.purchasedUsers.findIndex((purchaseInfo: any) => String(purchaseInfo.purchasedBy) === String(user.id));
-                        if (firstIndex !== -1) firstDate = first.purchasedUsers[firstIndex].purchasedAt;
-                        if (secondIndex !== -1) secondDate = second.purchasedUsers[secondIndex].purchasedAt;
-                        if (firstDate < secondDate) return 1;
-                        else if (firstDate > secondDate) return -1;
-                        else {
-                          if (first.date < second.date) return 1;
-                          else if (first.date > second.date) return -1;
-                          return 0;
-                        }
-                      } else {
-                        if (first.currency && second.currency === null) return 1;
-                        else if (first.currency === null && second.currency) return -1;
-                        else {
-                          if (first.date < second.date) return 1;
-                          else if (first.date > second.date) return -1;
-                          return 0;
-                        }
-                      }
-                    })
-                    .map((bite: any, index: any) => (
-                      <div className="profile-bite" key={index}>
-                        <BiteCardProfile bite={bite} same={isSame} />
-                      </div>
-                    ))}
-                </div>
-              ) : (
-                <div className="no-data">
-                  <span>There is no "Bite" yet </span>
-                  {code === 'mybites' && (
-                    <div
-                      style={{
-                        marginTop: "20px",
-                        display: "flex",
-                        justifyContent: "center",
-                      }}
-                    >
-                      <Button
-                        text="Create"
-                        fillStyle="fill"
-                        color="primary"
-                        shape="rounded"
-                        width={"300px"}
-                        icon={[
-                          <AddIcon color="white" />,
-                          <AddIcon color="white" />,
-                          <AddIcon color="white" />,
-                        ]}
-                        handleSubmit={() => navigate("/bite/create-type")}
-                      />
+                <div className="subscription-body">
+                  <div className="subscription-body-title">
+                    <span>Subscription</span>
+                  </div>
+                  <div className="subscription-body-main">
+                    <div className="image">
+                    <img src={subscriptionImg} alt="subscription" />
                     </div>
-                  )}
+                    <div className="description">
+                      <span>You can provide subscription-only benefits for subscribe members, such as additional creative content, subscription to member-only communities, etc.</span>
+                      <div className="subscription-btn">
+                        <span>Set subscription</span>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              )}
+              </>
+              }
+              <div className="subscription-title">
+                <NotificationOutlineIcon color="#EA8426" width={24} /><span>I have subscribed</span>
+              </div>
             </div>
+            :
+          <div className="creators-bite">
+          {(!isSame && authuser) &&
+            <div className="title">
+              <Bite1Icon color="#EFA058" width={30} height={30} />
+              <span>{authuser.name}’s Bite-sized knowledge</span>
+            </div>
+          }
+            {bites.length > 0 && authuser ? (
+              <div className="bite-card">
+                {bites.sort((first: any, second: any) => {
+                    if (user) {
+                      let firstDate: any = "2322";
+                      let secondDate: any = "2322";
+                      let firstIndex = first.purchasedUsers.findIndex((purchaseInfo: any) => String(purchaseInfo.purchasedBy) === String(user.id));
+                      let secondIndex = second.purchasedUsers.findIndex((purchaseInfo: any) => String(purchaseInfo.purchasedBy) === String(user.id));
+                      if (firstIndex !== -1) firstDate = first.purchasedUsers[firstIndex].purchasedAt;
+                      if (secondIndex !== -1) secondDate = second.purchasedUsers[secondIndex].purchasedAt;
+                      if (firstDate < secondDate) return 1;
+                      else if (firstDate > secondDate) return -1;
+                      else {
+                        if (first.date < second.date) return 1;
+                        else if (first.date > second.date) return -1;
+                        return 0;
+                      }
+                    } else {
+                      if (first.currency && second.currency === null) return 1;
+                      else if (first.currency === null && second.currency) return -1;
+                      else {
+                        if (first.date < second.date) return 1;
+                        else if (first.date > second.date) return -1;
+                        return 0;
+                      }
+                    }
+                  })
+                  .map((bite: any, index: any) => (
+                    <div className="profile-bite" key={index}>
+                      <BiteCardProfile bite={bite} same={isSame} />
+                    </div>
+                  ))}
+              </div>
+            ) : (
+              <div className="no-data">
+                <span>There is no "Bite" yet </span>
+                {code === 'mybites' && (
+                  <div
+                    style={{
+                      marginTop: "20px",
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Button
+                      text="Create"
+                      fillStyle="fill"
+                      color="primary"
+                      shape="rounded"
+                      width={"300px"}
+                      icon={[
+                        <AddIcon color="white" />,
+                        <AddIcon color="white" />,
+                        <AddIcon color="white" />,
+                      ]}
+                      handleSubmit={() => navigate("/bite/create-type")}
+                    />
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
         } 
       </div>
     </div>
