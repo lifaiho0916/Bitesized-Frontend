21bcc844822b666af066449d3cd58536e60de563
manishchandra-resiliencesoft <manishchandra@resiliencesoft.com>
2022-12-21 19:03:58 +0530

    Implemented instagram connect WIP

--- src/pages/profile/edit/InstagramLogin.tsx
+++ src/pages/profile/edit/InstagramLogin.tsx
@@ -3,94 +3,110 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { accountAction } from "../../../redux/actions/socialAccountActions";
 import IgBtn from "../../../assets/svg/ig.svg";
+import api from "@fortawesome/fontawesome";
+import axios from "axios";
 
 export const InstagramLogin = (props: any) => {
-  const { account, contexts } = props
-  const [searchParams] = useSearchParams();
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const userState = useSelector((state: any) => state.auth);
-  const { user } = userState;
+	const { account, contexts } = props;
+	const [searchParams] = useSearchParams();
+	const { pathname } = useLocation();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const userState = useSelector((state: any) => state.auth);
+	const { user } = userState;
 
-  const clickHandler = () => {
-    alert("We will launch it soon")
-    // const clientId = process.env.REACT_APP_INSTAGRAM_APP_ID;
-    // const scope = process.env.REACT_APP_INSTAGRAM_SCOPE;
-    // const responseType = "code";
-    // const redirectUri = `https://${window.location.host}${window.location.pathname}`;
-    // window.location.href = `https://api.instagram.com/oauth/authorize/?app_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
-  };
+	const clickHandler = () => {
+		//alert("We will launch it soon");
+		const clientId = process.env.REACT_APP_INSTAGRAM_APP_ID;
+		const scope = process.env.REACT_APP_INSTAGRAM_SCOPE;
+		const responseType = "code";
+		const redirectUri = `https://${window.location.host}${window.location.pathname}`;
+		window.location.href = `https://api.instagram.com/oauth/authorize/?app_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
+	};
 
-  const removeHandler = () => {
-    // const { _id } = accounts.find((acc: any) => acc.name === "instagram");
-    // dispatch(accountAction.removeAccount(_id));
-  };
+	const removeHandler = () => {
+		// const { _id } = accounts.find((acc: any) => acc.name === "instagram");
+		// dispatch(accountAction.removeAccount(_id));
+		if (hasInstagram)
+			dispatch(accountAction.removeAccount(account._id, "instagram"));
+	};
 
-  const hasInstagram: any = useMemo(() => {
-    if (account && account.social && account.social.instagram) return true
-    else return false
-  }, [account]);
+	const hasInstagram: any = useMemo(() => {
+		if (account && account.social && account.social.instagram) return true;
+		else return false;
+	}, [account]);
 
-  const onSuccess = useCallback(
-    async (code) => {
-      const redirect_uri = `https://${window.location.host}${window.location.pathname}`;
-      const meta = {
-        code,
-        redirect_uri,
-        grant_type: "authorization_code",
-      };
-      console.log(meta.redirect_uri);
-      const data = {
-        id: code,
-        name: "instagram",
-        metadata: JSON.stringify(meta),
-      };
-      // dispatch(
-      //   accountAction.addAccount(data, (result: Boolean) => {
-      //     if (result) {
-      //       // dispatch(accountAction.getAccounts(user.id));
-      //       setTimeout(() => {
-      //         navigate(pathname);
-      //       }, 2000);
-      //     }
-      //   })
-      // );
-    },
-    [dispatch, pathname, navigate]
-  );
+	const onSuccess = useCallback(
+		async (code) => {
+			try {
+				const redirect_uri = `https://${window.location.host}${window.location.pathname}`;
+				const meta = {
+					code,
+					redirect_uri,
+					grant_type: "authorization_code",
+				};
 
-  const openInstagramProfile = () => {
-    // const data = accounts.find((acc: any) => acc.name === "instagram");
-    // const { username } = JSON.parse(data.metadata);
-    // window.open(`https://www.instagram.com/${username}`);
-  };
+				const data = {
+					id: code,
+					name: "instagram",
+					metadata: JSON.stringify(meta),
+				};
 
-  const onFailure = useCallback(() => {
-    if (searchParams.get("error")) {
-      const data = {
-        error: searchParams.get("error"),
-        error_reason: searchParams.get("error_reason"),
-        error_description: searchParams.get("error_description"),
-      };
-      console.log(data);
-    }
-  }, [searchParams]);
+				dispatch(accountAction.addInstagramAccount(data));
+			} catch (error) {
+				alert("Something went wrong");
+				console.log(error);
+			}
 
-  useEffect(() => {
-    const code = searchParams.get("code");
-    if (code) onSuccess(code);
-    else onFailure();
-  }, [onSuccess, onFailure, searchParams, dispatch, user]);
+			// dispatch(
+			//   accountAction.addAccount(data, (result: Boolean) => {
+			//     if (result) {
+			//       // dispatch(accountAction.getAccounts(user.id));
+			//       setTimeout(() => {
+			//         navigate(pathname);
+			//       }, 2000);
+			//     }
+			//   })
+			// );
+		},
+		[dispatch, pathname, navigate]
+	);
 
-  return (
-    <div
-      className={hasInstagram ? "remove-btn" : "connect-btn"}
-      onClick={hasInstagram ? removeHandler : clickHandler}
-    >
-      {hasInstagram ? <span>Remove</span> : <span>{contexts.GENERAL.CONNECT}</span>}
-    </div>
-  );
+	const openInstagramProfile = () => {
+		const data = accounts.find((acc: any) => acc.name === "instagram");
+		const { username } = JSON.parse(data.metadata);
+		window.open(`https://www.instagram.com/${username}`);
+	};
+
+	const onFailure = useCallback(() => {
+		if (searchParams.get("error")) {
+			const data = {
+				error: searchParams.get("error"),
+				error_reason: searchParams.get("error_reason"),
+				error_description: searchParams.get("error_description"),
+			};
+			console.log(data);
+		}
+	}, [searchParams]);
+
+	useEffect(() => {
+		const code = searchParams.get("code");
+		if (code) onSuccess(code);
+		else onFailure();
+	}, [onSuccess, onFailure, searchParams, dispatch, user]);
+
+	return (
+		<div
+			className={hasInstagram ? "remove-btn" : "connect-btn"}
+			onClick={hasInstagram ? removeHandler : clickHandler}
+		>
+			{hasInstagram ? (
+				<span>Remove</span>
+			) : (
+				<span>{contexts.GENERAL.CONNECT}</span>
+			)}
+		</div>
+	);
 };
 
 export default InstagramLogin;
--- src/redux/actions/socialAccountActions.tsx
+++ src/redux/actions/socialAccountActions.tsx
@@ -1,54 +1,72 @@
-import { Dispatch } from 'redux';
-import * as api from '../../api';
+import { Dispatch } from "redux";
+import * as api from "../../api";
 import {
-  SET_SOCIAL_ACCOUNT,
-  SET_LOADING_FALSE,
-  SET_LOADING_TRUE
-} from '../types';
+	SET_SOCIAL_ACCOUNT,
+	SET_LOADING_FALSE,
+	SET_LOADING_TRUE,
+} from "../types";
 export const accountAction = {
-  getAccount: (userId: any) => async (dispatch: Dispatch<any>) => {
-    try {
-      dispatch({ type: SET_SOCIAL_ACCOUNT, payload: null })
-      const response = await api.getSocialAccount(userId)
-      const { data } = response
-      
-      if(data.success) {
-        const { payload } = data
-        dispatch({ type: SET_SOCIAL_ACCOUNT, payload: payload.socialAccount})
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  addAccount: (socialInfo: any) => async (dispatch: Dispatch<any>) => {
-      try {
-        dispatch({ type: SET_LOADING_TRUE })
-        const response = await api.addSocialAccount(socialInfo)
-        const { data } = response
-        dispatch({ type: SET_LOADING_FALSE })
-        if(data.success) {
-          const { payload } = data
-          alert("Youtube account is connected successfully")
-          dispatch({ type: SET_SOCIAL_ACCOUNT, payload: payload.socialAccount });
-        }
-      } catch (error) {
-        dispatch({ type: SET_LOADING_FALSE })
-        console.log(error)
-      }
-    },
-  removeAccount: (id: any, social: any) => async (dispatch: Dispatch<any>) => {
-    try {
-      dispatch({ type: SET_LOADING_TRUE })
-      const response = await api.removeSocialAccount(id, social)
-      const { data } = response
-      dispatch({ type: SET_LOADING_FALSE })
-      if(data.success) {
-        const { payload } = data
-        dispatch({ type: SET_SOCIAL_ACCOUNT, payload: payload.socialAccount })
-      }
-    } catch (error) {
-      dispatch({ type: SET_LOADING_FALSE })
-      console.log(error);
-    }
-  },
+	getAccount: (userId: any) => async (dispatch: Dispatch<any>) => {
+		try {
+			dispatch({ type: SET_SOCIAL_ACCOUNT, payload: null });
+			const response = await api.getSocialAccount(userId);
+			const { data } = response;
+
+			if (data.success) {
+				const { payload } = data;
+				dispatch({ type: SET_SOCIAL_ACCOUNT, payload: payload.socialAccount });
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	},
+	addAccount: (socialInfo: any) => async (dispatch: Dispatch<any>) => {
+		try {
+			dispatch({ type: SET_LOADING_TRUE });
+			const response = await api.addSocialAccount(socialInfo);
+			const { data } = response;
+			dispatch({ type: SET_LOADING_FALSE });
+			if (data.success) {
+				const { payload } = data;
+				alert("Youtube account is connected successfully");
+				dispatch({ type: SET_SOCIAL_ACCOUNT, payload: payload.socialAccount });
+			}
+		} catch (error) {
+			dispatch({ type: SET_LOADING_FALSE });
+			console.log(error);
+		}
+	},
+	removeAccount: (id: any, social: any) => async (dispatch: Dispatch<any>) => {
+		try {
+			dispatch({ type: SET_LOADING_TRUE });
+			const response = await api.removeSocialAccount(id, social);
+			const { data } = response;
+			dispatch({ type: SET_LOADING_FALSE });
+			if (data.success) {
+				const { payload } = data;
+				dispatch({ type: SET_SOCIAL_ACCOUNT, payload: payload.socialAccount });
+			}
+		} catch (error) {
+			dispatch({ type: SET_LOADING_FALSE });
+			console.log(error);
+		}
+	},
+	addInstagramAccount: (socialInfo: any) => async (dispatch: Dispatch<any>) => {
+		try {
+			dispatch({ type: SET_LOADING_TRUE });
+			const response = await api.addInstagramAccount(socialInfo);
+			const { data } = response;
+			console.log("payload", data);
+			dispatch({ type: SET_LOADING_FALSE });
+			if (data.success) {
+				const { payload } = data;
+
+				alert("Instagram account is connected successfully");
+				dispatch({ type: SET_SOCIAL_ACCOUNT, payload: payload.socialAccount });
+			}
+		} catch (error) {
+			dispatch({ type: SET_LOADING_FALSE });
+			console.log(error);
+		}
+	},
 };
